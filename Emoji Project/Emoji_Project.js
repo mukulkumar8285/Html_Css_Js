4d81c6dbcd8bc4c4aa85a5a22330af7a5ebf6cbc
Mukul Kumar <149408801+mukulkumar8285@users.noreply.github.com>
2024-01-18 00:28:36 +0530

    addagain

--- Emoji Project/Emoji_Project.js
+++ Emoji Project/Emoji_Project.js
@@ -1,43 +1,43 @@
-function searchEmoji(){ontainer
+function findemoji(){
     let inputvalue = document.getElementById("input_text").value;
-    displayResult(inputvalue);
+    findresult(inputvalue);
 }
-function displayResult(searchQuery=""){
-    let datafilter = emojiList.filter((e)=>{
-        if(e.description.indexOf(searchQuery) != -1){
+function findresult(qure=""){
+    let filterData = emojiList.filter((e)=>{
+        if(e.description.indexOf(qure) != -1){
             return true;
         }
-        if(e.tags.some(elem=>elem.startsWith(searchQuery))){
+        if(e.tags.some(elem=>elem.startsWith(qure))){
             return true;
         }
-        if(e.aliases.some(elem=>elem.startsWith(searchQuery))){
+        if(e.aliases.some(elem=>elem.startsWith(qure))){
             return true;
         }
 
        
     })
-    console.log(datafilter);
+    console.log(filterData);
     let parent = document.getElementById("search");
     console.log(parent);
     parent.innerHTML="";
 
 
-    datafilter.forEach((e)=>{
-        let new_row = document.createElement("tr");
-        let new_emoji = document.createElement("td");
-        let new_aliases = document.createElement("td");
-        let new_dese = document.createElement("td");
+    filterData.forEach((e)=>{
+        let row = document.createElement("tr");
+        let emoji = document.createElement("td");
+        let alise = document.createElement("td");
+        let dese = document.createElement("td");
         
-        new_emoji.innerText = e.emoji;
-        new_aliases.innerText = e.aliases;
-        new_dese.innerText= e.description;
+        emoji.innerText = e.emoji;
+        alise.innerText = e.aliases;
+        dese.innerText= e.description;
 
-        new_row.appendChild(new_emoji);
-        new_row.appendChild(new_aliases);
-        new_row.appendChild(new_dese);
+        row.appendChild(emoji);
+        row.appendChild(alise);
+        row.appendChild(dese);
 
-        parent.append(new_row);
+        parent.append(row);
     });
 }
-document.getElementById("input_text").addEventListener("keyup" , searchEmoji)
-window.onload= ()=>displayResult();
+document.getElementById("input_text").addEventListener("keyup" , findemoji)
+window.onload= ()=>findresult();
