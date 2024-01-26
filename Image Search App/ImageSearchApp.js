a76c60d4f4306c84e834198b3f546eb035703b6c
Mukul Kumar <149408801+mukulkumar8285@users.noreply.github.com>
2024-01-27 00:28:55 +0530

    again add

--- Image Search App/ImageSearchApp.js
+++ Image Search App/ImageSearchApp.js
@@ -1,21 +1,26 @@
-function SearchPhoto(){
-let my_id = "UixsqNPFiP_VfxnzZ005M9GcaY2wLUOs3QTXlLsNq-c";
+function SearchPhoto() {
+    let my_id = "UixsqNPFiP_VfxnzZ005M9GcaY2wLUOs3QTXlLsNq-c";
 
-let query  =document.getElementById("input").value;
-let url = "https://api.unsplash.com/search/photos?client_id=" + my_id + "&query="+query;
+    let query = document.getElementById("input").value;
+    let url = "https://api.unsplash.com/search/photos?client_id=" + my_id + "&query=" + query;
 
-fetch(url)
-    .then(function(data){
-        return data.json();
-    })
-.then(function(data){
-    console.log(data);
-    data.results.forEach(photo => {
-        let result = `
-        <img src = "${photo.urls.regular}"> `;
-        $("#result").append(result);
-        
-    });
-});
+    fetch(url)
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (data) {
+            let resultsHTML = ""; // Initialize an empty string to store the HTML content
 
+            data.results.forEach(photo => {
+                resultsHTML += `
+                    <div class="photo-container">
+                        <img src="${photo.urls.regular}" alt="${photo.alt_description}">
+                        <span class="description">${photo.alt_description}</span>
+                    </div>
+                `;
+            });
+
+            // Update the HTML outside the loop
+            document.getElementById("result").innerHTML = resultsHTML;
+        });
 }
